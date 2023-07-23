d6dd164e47089a80df1fcf2ef58c1290d776658a
Niko <soder.n@gmail.com>
2023-07-23 19:10:08 +0300

    Install tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import "./App.css";
 
-function App() {
+const App = () => {
   return (
     <>
-      <p>hello world</p>
+      <h1 className="m-10 p-3 text-3xl font-bold text-red-200 underline hover:bg-blue-800 hover:text-red-800">
+        Hello world!
+      </h1>
     </>
   );
-}
+};
 
 export default App;
