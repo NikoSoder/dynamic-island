502e6bdd0780ac19b9b41d1e70bed7d12df66373
Niko <soder.n@gmail.com>
2023-07-25 13:29:13 +0300

    Style Timer

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import "./App.css";
 import Idle from "./components/Idle";
 import Ring from "./components/Ring";
 import { useState } from "react";
+import Timer from "./components/Timer";
 
 const App = () => {
   const [active, setActive] = useState("Idle");
@@ -11,6 +12,7 @@ const App = () => {
         <div className="h-52 rounded-t-lg border bg-white p-6 transition-all">
           {active === "Idle" && <Idle />}
           {active === "Ring" && <Ring />}
+          {active === "Timer" && <Timer />}
         </div>
         <div className="rounded-b-lg border-x border-b bg-slate-50 p-4">
           <button
@@ -24,6 +26,12 @@ const App = () => {
             className="rounded border bg-white px-4 py-2 text-sm font-semibold"
           >
             Ring Mode
+          </button>
+          <button
+            onClick={() => setActive("Timer")}
+            className="rounded border bg-white px-4 py-2 text-sm font-semibold"
+          >
+            Timer
           </button>
         </div>
       </div>
---
+++ src/components/Timer.tsx
@@ -0,0 +1,64 @@
+import { PauseIcon } from "@heroicons/react/24/solid";
+import { XMarkIcon } from "@heroicons/react/24/solid";
+import { PlayIcon } from "@heroicons/react/24/solid";
+import { useState, useEffect } from "react";
+
+const Timer = () => {
+  const [pause, setPause] = useState(false);
+  const [counter, setCounter] = useState(0);
+
+  const clear = () => {
+    setCounter(0);
+  };
+
+  const handlePauseToggle = () => {
+    setPause(!pause);
+  };
+
+  useEffect(() => {
+    console.log(counter);
+    const interval = setInterval(() => {
+      if (!pause) {
+        setCounter(counter + 1);
+      }
+    }, 1000);
+
+    return () => clearInterval(interval);
+  });
+
+  useEffect(() => {
+    if (counter === 60) {
+      clear();
+    }
+  }, [counter]);
+
+  return (
+    <div className="text-md flex w-52 flex-row items-center justify-between rounded-full bg-black p-3">
+      <div className="flex flex-row gap-1">
+        <div className="cursor-pointer rounded-full bg-yellow-800 p-1 hover:bg-yellow-700">
+          {!pause && (
+            <PauseIcon
+              onClick={handlePauseToggle}
+              className="h-6 w-6 text-yellow-400"
+            />
+          )}
+          {pause && (
+            <PlayIcon
+              onClick={handlePauseToggle}
+              className="h-6 w-6 text-yellow-400"
+            />
+          )}
+        </div>
+        <div className="cursor-pointer rounded-full bg-gray-600 p-1 hover:bg-gray-500">
+          <XMarkIcon onClick={clear} className="h-6 w-6 text-white" />
+        </div>
+      </div>
+      <div className="flex flex-row items-center gap-1 text-yellow-400">
+        <p>Timer</p>
+        <p className="text-2xl">0:{counter >= 10 ? counter : `0${counter}`}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Timer;
