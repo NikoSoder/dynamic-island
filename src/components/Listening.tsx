eabb6298c2e1fada7dd7aac4e8cd2cf8b59e4997
Niko <soder.n@gmail.com>
2023-07-27 13:03:38 +0300

    Move listening animation to own component

--- src/components/Listening.tsx
+++ src/components/Listening.tsx
@@ -4,6 +4,7 @@ import { PlayIcon } from "@heroicons/react/24/solid";
 import { BackwardIcon } from "@heroicons/react/24/solid";
 import { ForwardIcon } from "@heroicons/react/24/solid";
 import { useState, useEffect } from "react";
+import ListeningSongAnimation from "./ListeningSongAnimation";
 
 const Listening = () => {
   const [pause, setPause] = useState(false);
@@ -53,20 +54,13 @@ const Listening = () => {
           <p>Never Gonna Give You Up</p>
           <p className="text-slate-400">Rick Astley</p>
         </div>
-        <div className="flex items-center justify-center gap-1">
-          <div className="listening-bar-animation h-2 w-[1px] bg-yellow-600"></div>
-          <div className="listening-bar-animation h-4 w-[1px] bg-yellow-700"></div>
-          <div className="listening-bar-animation h-5 w-[1px] bg-yellow-600"></div>
-          <div className="listening-bar-animation h-6 w-[1px] bg-yellow-600"></div>
-          <div className="listening-bar-animation h-5 w-[1px] bg-yellow-600"></div>
-          <div className="listening-bar-animation h-4 w-[1px] bg-yellow-700"></div>
-          <div className="listening-bar-animation h-2 w-[1px] bg-yellow-600"></div>
-        </div>
+        <ListeningSongAnimation pause={pause} />
       </section>
       <section className="flex items-center justify-between text-slate-400">
         <p>
           {minutes}:{seconds >= 10 ? seconds : `0${seconds}`}
         </p>
+        {/* song time remaining bar */}
         <div className="h-1 w-52 rounded-md bg-slate-700">
           <div
             style={{ animationPlayState: !pause ? "running" : "paused" }}
---
+++ src/components/ListeningSongAnimation.tsx
@@ -0,0 +1,36 @@
+const ListeningSongAnimation = ({ pause }: { pause: boolean }) => {
+  return (
+    <div className="flex items-center justify-center gap-1">
+      <div
+        style={{ animationPlayState: !pause ? "running" : "paused" }}
+        className="listening-bar-animation h-2 w-[1px] bg-yellow-600"
+      ></div>
+      <div
+        style={{ animationPlayState: !pause ? "running" : "paused" }}
+        className="listening-bar-animation h-4 w-[1px] bg-yellow-700"
+      ></div>
+      <div
+        style={{ animationPlayState: !pause ? "running" : "paused" }}
+        className="listening-bar-animation h-5 w-[1px] bg-yellow-600"
+      ></div>
+      <div
+        style={{ animationPlayState: !pause ? "running" : "paused" }}
+        className="listening-bar-animation h-6 w-[1px] bg-yellow-600"
+      ></div>
+      <div
+        style={{ animationPlayState: !pause ? "running" : "paused" }}
+        className="listening-bar-animation h-5 w-[1px] bg-yellow-600"
+      ></div>
+      <div
+        style={{ animationPlayState: !pause ? "running" : "paused" }}
+        className="listening-bar-animation h-4 w-[1px] bg-yellow-700"
+      ></div>
+      <div
+        style={{ animationPlayState: !pause ? "running" : "paused" }}
+        className="listening-bar-animation h-2 w-[1px] bg-yellow-600"
+      ></div>
+    </div>
+  );
+};
+
+export default ListeningSongAnimation;
